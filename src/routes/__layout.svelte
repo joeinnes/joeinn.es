19f59bf7457fd64bc586a6b21c0637a0f010a5e2
Joe Innes <joe@joeinn.es>
2021-11-05 10:18:18 +0100

    Initial working version

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,8 @@
 <script>
-  import Seo from "$lib/Seo.svelte";
-  import "../app.postcss";
-  import "../style.css";
+  import '../styles/tailwind-output.css';
+  import '../style.css';
 </script>
 
-<Seo />
 <div class="body-wrapper max-w-screen-md mx-auto">
   <slot />
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,20 @@
-import preprocess from "svelte-preprocess";
-import { mdsvex } from "mdsvex";
-import { mdsvexConfig } from "./mdsvex.config.js";
-import adapter from "@sveltejs/adapter-netlify";
+import preprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex';
+import { mdsvexConfig } from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [".svelte", ...mdsvexConfig.extensions],
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
   preprocess: [
     preprocess({
-      postcss: true,
+      postcss: true
     }),
-    mdsvex(mdsvexConfig),
+    mdsvex(mdsvexConfig)
   ],
   kit: {
-    adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
-  },
+    target: '#svelte'
+  }
 };
 
 export default config;
