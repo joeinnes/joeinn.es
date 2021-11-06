95df93ba5fd293cd345782acd56591c4a6f0d772
Joe Innes <joe@joeinn.es>
2021-11-06 20:36:17 +0100

    Update template to show featured image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,11 +24,14 @@
   <article>
     <h1 class="headline text-7xl leading-relaxed font-black font-display mb-4">Joe Innes</h1>
     <div class="article-list">
-      {#each items as { metadata: { title, tags, outline, slug }, path, content }}
+      {#each items as { metadata: { title, tags, featured_image, slug }, path, content }}
         <div class="mb-4">
           <a sveltekit:prefetch href={path.replace(/\.[^/.]+$/, '')}
             ><h2 class="text-3xl leading-relaxed">{title}</h2></a
           >
+          {#if featured_image}
+            <img class="mb-4" src={featured_image.url} alt={featured_image.alt} />
+          {/if}
           {@html truncate(content, 30, { byWords: true })}
         </div>
       {/each}
