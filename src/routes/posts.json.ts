32bfbdd01fd1a3df6ce6bad1bc30da84eb4b4967
Joe Innes <joe@joeinn.es>
2021-11-06 20:31:50 +0100

    Sort by publish date

--- src/routes/posts.json.ts
+++ src/routes/posts.json.ts
@@ -18,6 +18,11 @@ export async function get() {
 
   const posts = await Promise.all(body);
 
+  posts.sort((a, b) => {
+    console.log(a);
+    return new Date(a.metadata.date_published).getTime() < new Date(b.metadata.date_published).getTime() ? 1 : -1
+  });
+
   return {
     body: JSON.stringify(posts)
   };
