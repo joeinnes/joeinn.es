7345e25241652eb5998c39f6000ce93b6e4cee65
Joe Innes <joe@joeinn.es>
2025-02-24 15:30:28 +0100

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -11,7 +11,7 @@
 			<li><a href="/blog">Blog</a></li>
 			<li><a href="/cv">CV</a></li>
 			<li><a href="/contact">Contact</a></li>
-			<li><a href="/til">TIL</a></li>
+			<li><a href="/smidgeons">Smidgeons</a></li>
 			<li><a href="/now">Now</a></li>
 		</ul>
 	</div>
