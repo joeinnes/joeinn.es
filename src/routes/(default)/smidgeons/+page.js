ae97101e670a6ac4257d4a81853812ab23448051
Joe Innes <joe@joeinn.es>
2025-06-23 14:54:31 +0200

    Delete everything

--- src/routes/(default)/smidgeons/+page.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export const load = async ({ fetch }) => {
-	const fetchedTils = await fetch(
-		'https://api.joeinn.es/api/collections/til/records?sort=-created'
-	);
-	const tils = await fetchedTils.json();
-	return tils;
-};
